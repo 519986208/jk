5872f311c7caa4ace91c16d61b534ab82d7d93dc
chengliyao <chengliyao@f-road.com.cn>
2019-12-17 10:43:31 +0800

    13

--- src/main/java/com/ahhf/cly/Fda/App.java
+++ src/main/java/com/ahhf/cly/Fda/App.java
@@ -7,8 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @SpringBootApplication
+@Slf4j
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
@@ -16,6 +19,7 @@ public class App {
 
     @GetMapping("test123")
     public Object fa() {
+        log.info("当前的时间啊11111111111：" + new Date().toLocaleString());
         return "当前的时间啊11111111111：" + new Date().toLocaleString();
     }
 }
